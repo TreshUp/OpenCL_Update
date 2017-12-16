60e7fb3db4bd45fc7ead0c5f077c961a21253c02
TreshUP <tresh500@gmail.com>
2017-12-16 20:20:05 +0500

    STABLE V IN DEL

--- Kurs/Hello.cl
+++ Kurs/Hello.cl
@@ -1,45 +1,35 @@
 __kernel void square_fwd1(__global double* Value, __global int* Col, __global int* RowIndex, int j,__global float* koef) 
  { 
-	 //Value[RowIndex[j]]=sqrt(fabs(Value[RowIndex[j]]));
 	 koef[(((1+j)*j)/2)+j]=sqrt(fabs(Value[RowIndex[j]]));
  }
-__kernel void square_fwd2(__global double* Value, __global int* Col, __global int* RowIndex, int i,__global float* koef, __global float* tmp)//int j) 
+__kernel void square_fwd2(__global double* Value, __global int* Col, __global int* RowIndex, int i,__global float* koef)
  { 
 	float s;
 	int j = get_global_id(0); 
 	 if ((j)>i)
 	 { 
-		 //s = Value[RowIndex[i]+j - Col[RowIndex[i]]];
 		if (j==(Col[RowIndex[i]+j - Col[RowIndex[i]]]))
 		{
 			s = Value[RowIndex[i]+j - Col[RowIndex[i]]];
 			for(int k = 0; k < i; k++)
 			{
-				//s -= Value[RowIndex[k]+j - Col[RowIndex[k]]]*Value[RowIndex[k]+i - Col[RowIndex[k]]];
 				s-=koef[(((1+j)*j)/2)+k]*koef[(((1+i)*i)/2)+k];
-				//s[0]-=koef[(((1+k)*k)/2)+j]*koef[(((1+k)*k)/2)+i];
-				///s-=koef[(((1+k)*k)/2)+j]*koef[(((1+k)*k)/2)+i];
 			} 
  
 		}
 		else s=0;
-			//s/=Value[RowIndex[i]];
 			s/=koef[(((1+i)*i)/2)+i];
 		
-			//Value[RowIndex[i]+j - Col[RowIndex[i]]] = s;
 			koef[(((1+j)*j)/2)+i]=s;
-			///koef[(((1+i)*i)/2)+j]=s;
 			Value[RowIndex[j]] -= s*s;
      }
  }
-__kernel void square_y1(__global double* Value, __global int* Col, __global int* RowIndex, __global float* x,__global float* Sv, int i,__global float* koef) 
+__kernel void square_y1(__global float* x,__global float* Sv, int i,__global float* koef) 
  { 
-     //x[i] = U[n-1+i*n] / U[i+i*n]; 
-	 ////x[i]=Sv[i]/Value[RowIndex[i]];
+
 	 x[i]=Sv[i]/koef[(((1+i)*i)/2)+i];
-	 //x[i]=111;
  } 
-__kernel void square_y2(__global double* Value, __global int* Col, __global int* RowIndex, __global float* x,__global float* Sv, int i,__global float* koef) 
+__kernel void square_y2(__global float* x,__global float* Sv, int i,__global float* koef) 
  { 
      local float y; 
      y = x[i]; 
@@ -50,23 +40,18 @@ __kernel void square_y2(__global double* Value, __global int* Col, __global int*
 
      if(k > i)
 	 { 
-         //U[n-1+k*n] -= U[k+i*n] * y; 
-		 //Sv[k]-=Sv[i]*y;
-		 //Sv[k]-=koef[(((1+k)*k)/2)+i]*y;
 		 Sv[k]-=y * koef[(((1+k)*k)/2)+i];
      } 
  } 
  
  
-__kernel void square_x1(__global double* Value, __global int* Col, __global int* RowIndex, __global float *x, int i,__global float* koef) 
+__kernel void square_x1(__global float *x, int i,__global float* koef) 
  { 
-     //x[i] /= U[i+i*n];
-	//x[i]/= Value[RowIndex[i]];
 	x[i]/=koef[(((1+i)*i)/2)+i];
  } 
  
  
-__kernel void square_x2(__global double* Value, __global int* Col, __global int* RowIndex, __global float *x, int i,__global float* koef) 
+__kernel void square_x2(__global float *x, int i,__global float* koef) 
  { 
      local float y; 
      y = x[i]; 
@@ -75,8 +60,6 @@ __kernel void square_x2(__global double* Value, __global int* Col, __global int*
      int k = get_global_id(0); 
 	if (k<i)
 	{
-		//x[k] -= y * U[i+k*n]; 
-		//x[k]-=y*Value[RowIndex[k]+i - Col[RowIndex[k]]];
 		x[k]-=y*koef[(((1+i)*i)/2)+k];
 	 }
  }
--- Kurs/Kurs.cpp
+++ Kurs/Kurs.cpp
@@ -26,89 +26,27 @@ struct crsMatrix
 
 void InitializeMatrix(crsMatrix &mtx)
 {
+
 	mtx.Value= (double*)calloc(local_NZ*N, sizeof(double));
 	mtx.Col = (int*)calloc(local_NZ*N, sizeof(int));
 	mtx.RowIndex = (int*)calloc(N + 1, sizeof(int));
 }
-void FreeMemory(crsMatrix &mtx)
+void FreeMemory(crsMatrix &mtx, cl_platform_id * platforms, cl_device_id *device_list, cl_mem Value_clmem, cl_mem Col_clmem, cl_mem Row_Index_clmem)
 {
+
 	free(mtx.Value);
 	free(mtx.Col);
 	free(mtx.RowIndex);
-}
-void Generate(crsMatrix &mtx)
-{
-	int* Num_Non_zero;
-	Num_Non_zero = new int[N];
-	for (int c = 0; c < N; c++)
-	{
-		Num_Non_zero[c] = local_NZ;
-	}
-	ofstream fout;
-	fout.setf(ios::fixed);
-	fout.precision(1);
-	fout.open("MTX.txt", ios::out);
-	int count = 0;
-	mtx.RowIndex[0] = 0;
-	srand(time(NULL));
-	int i,j = 0;
-	for (i = 0; i < N; i++)
-	{
-		int k = 0;
-		int	tmp = i;
-		int flag = 1;
-		while (k < Num_Non_zero[i])
-		{
-			if (flag == 1)
-			{
-				mtx.Value[j] = rand() % 5 + 1;
-				fout << " " << mtx.Value[j];
-				mtx.Col[j] = i;
-				fout << "( " << mtx.Col[j] << " )";
-				count++;
-				j++;
-				k++;
-				flag = 0;
-				Num_Non_zero[i]--;
-				//tmp = i;
-			}
-			if (Num_Non_zero[i] != 0)
-			{
-				if ((N - 1 - tmp) <= local_NZ)
-				{
-					//
-					if (i + local_NZ >= N) tmp = N - 1;
-					else tmp = i + local_NZ;
-
-				}
-				else tmp = tmp + rand() % ((N - tmp) / 3) + 1;
-				mtx.Value[j] = rand() % 5 + 1;
-				fout << " " << mtx.Value[j];
-				mtx.Col[j] = tmp;
-				fout << "( " << mtx.Col[j] << " )";
-				count++;
-				Num_Non_zero[tmp]--;
-				j++;
-				k++;
-			}
-		}
-		/*if (Num_Non_zero[i] == 0)
-		{
-			mtx.Value[j] = 0;
-			fout << " " << mtx.Value[j];
-			mtx.Col[j] = i;
-			fout << "( " << mtx.Col[j] << " )";
-		}*/
-		fout << endl;
-		mtx.RowIndex[i+1] = count;
-	}
-	fout << "end";
-	fout.close();
+	free(platforms);
+	free(device_list);
+	clReleaseMemObject(Value_clmem);
+	clReleaseMemObject(Col_clmem);
+	clReleaseMemObject(Row_Index_clmem);
 }
 
 void Len_Generate(crsMatrix &mtx)
 {
-
+	srand(time(NULL));
 	ofstream fout;
 	fout.setf(ios::fixed);
 	fout.precision(0);
@@ -159,10 +97,7 @@ void Len_Generate(crsMatrix &mtx)
 	}
 	fout << mtx.Value[j1] << "( " << mtx.Col[j] << " ) " << endl;
 	fout.close();
-	/*if (i = 0; i < N; i++)
-	{
-		if (mtx.Value[mtx.RowIndex[i]])
-	}*/
+
 }
 void Maple_check(crsMatrix &mtx)
 {
@@ -172,26 +107,20 @@ void Maple_check(crsMatrix &mtx)
 	{
 		Check[k]= (float*)calloc(N, sizeof(float));
 	}
-	//for (int i = 0; i < N; i++)
-	//{
-		int i = 0;
-		while (i < N)
+	int i = 0;
+	while (i < N)
+	{
+		int position = mtx.RowIndex[i];
+		int j = i;
+		while (position <= (mtx.RowIndex[i + 1] - 1))
 		{
-			int position = mtx.RowIndex[i];
-			//for (int j = 0; j < N; j++)
-			//{
-			int j = i;
-			while (position <= (mtx.RowIndex[i + 1] - 1))
-			{
-				Check[i][j] = mtx.Value[position];
-				if (j > i) Check[j][i] = mtx.Value[position];
-				position++;
-				j++;
-			}
-			i++;
+			Check[i][j] = mtx.Value[position];
+			if (j > i) Check[j][i] = mtx.Value[position];
+			position++;
+			j++;
 		}
-		//}
-	//}
+		i++;
+	}
 	ofstream fout;
 	fout.open("Sv.txt", ios::out);
 	for (int i = 0; i < N; i++)
@@ -219,12 +148,8 @@ int main()
 	//Выделение памяти
 	InitializeMatrix(mtx);
 
-	Generate(mtx);
 	Len_Generate(mtx);
-	/*for (int i = 0; i < N + 1; i++)
-	{
-		cout<<mtx.RowIndex[i]<<endl;
-	}*/
+
 #pragma region OpenCL
 #pragma region READ
 	//Это все для чтения кернела из файла Hello.cl
@@ -256,8 +181,7 @@ int main()
 	{
 		Sv[i] = i + 1;
 	}
-	float s[1];
-	//cout << "GET AND SET PLATFORM" << endl;
+
 	// Get platform and device information
 	cl_platform_id * platforms = NULL; //массив найденных платформ
 	cl_uint num_platforms; //кол-во платформ
@@ -274,14 +198,13 @@ int main()
 	device_list = (cl_device_id *)malloc(sizeof(cl_device_id)*num_devices);//динамическое выделение памяти
 	clStatus = clGetDeviceIDs(platforms[0], CL_DEVICE_TYPE_GPU, num_devices, device_list, NULL); //получение списка доступных устройств; на выходе в массиве содер. все устройства
 
-	//cout << "CREATE CONTEXT" << endl;
 	// Create one OpenCL context for each device in the platform
 	cl_context context;
 	context = clCreateContext(NULL, num_devices, device_list, NULL, NULL, &clStatus); //создание контекста
 	// Create a command queue
 	cl_command_queue command_queue = clCreateCommandQueue(context, device_list[0], 0, &clStatus); // создание очереди из команд для устройства;properties - список свойств очереди команд
 
-	//cout << "CREATE MEMORY" << endl;
+
 	// Create memory buffers on the device for each vector
     //выделение памяти для каждого из векторов
 	cl_mem Value_clmem = clCreateBuffer(context, CL_MEM_READ_WRITE, local_NZ*N * sizeof(double), NULL, &clStatus);
@@ -290,8 +213,7 @@ int main()
 	cl_mem X_clmem = clCreateBuffer(context, CL_MEM_WRITE_ONLY, N * sizeof(float), NULL, &clStatus);
 	cl_mem Sv_clmem= clCreateBuffer(context, CL_MEM_READ_WRITE, N * sizeof(float), NULL, &clStatus);
 	cl_mem koef_clmem= clCreateBuffer(context, CL_MEM_READ_WRITE, ((1 + N)*N) / 2 * sizeof(float), NULL, &clStatus);
-	cl_mem s_clmem = clCreateBuffer(context, CL_MEM_READ_WRITE, 1* sizeof(float), NULL, &clStatus);
-	//cout << "COPY MATRIX TO DEVICE" << endl;
+
 	// Copy the Buffer A and B to the device
 	//копирование векторов А и В из памяти хоста в буфер; CL_TRUE - вектор А мб использован и после вызова функции clEnqueueWriteBuffer
 	//мб меньше копировать????
@@ -299,7 +221,6 @@ int main()
 	clStatus = clEnqueueWriteBuffer(command_queue, Col_clmem, CL_TRUE, 0, local_NZ*N * sizeof(int), mtx.Col, 0, NULL, NULL);
 	clStatus = clEnqueueWriteBuffer(command_queue, Row_Index_clmem, CL_TRUE, 0, (N + 1) * sizeof(int), mtx.RowIndex, 0, NULL, NULL);
 
-	//cout << "CREATE 2 PROGRAMMS" << endl;
 	// Create a program from the kernel source
 	//создание программы, используя контекст, код функции-вычисления
 	cl_program program1 = clCreateProgramWithSource(context, 1, (const char **)&source_str, (const size_t *)&source_size, &clStatus);
@@ -309,7 +230,6 @@ int main()
 	cl_program program5 = clCreateProgramWithSource(context, 1, (const char **)&source_str, (const size_t *)&source_size, &clStatus);
 	cl_program program6 = clCreateProgramWithSource(context, 1, (const char **)&source_str, (const size_t *)&source_size, &clStatus);
 
-	//cout << "BUILD PROGRAM" << endl;
 	// Build the program
 	//компилирование и линковка программы
 	//первый параметр - программа; второй -число устройств; третий - список устройств; пятый - If pfn_notify is NULL, clBuildProgram does not return until the build has completed.
@@ -320,7 +240,7 @@ int main()
 	clStatus = clBuildProgram(program5, 1, device_list, NULL, NULL, NULL);
 	clStatus = clBuildProgram(program6, 1, device_list, NULL, NULL, NULL);
 	
-	//cout << "CREATE 2 KERNELS" << endl;
+
 	// Create the OpenCL kernel
 	//второй параметр - название функции, объявленной в программе как __kernel; 
 	cl_kernel kernel_fwd1 = clCreateKernel(program1, "square_fwd1", &clStatus);
@@ -330,168 +250,85 @@ int main()
 	cl_kernel kernel_sqr_x1 = clCreateKernel(program2, "square_x1", &clStatus);
 	cl_kernel kernel_sqr_x2 = clCreateKernel(program2, "square_x2", &clStatus);
 
-	//cout << "ARGUMENTS" << endl;
+
 	// Set the arguments of the kernel
-	//clStatus = clSetKernelArg(kernel_fwd1, 0, sizeof(cl_mem), (void *)&Value_clmem);
+	clStatus = clSetKernelArg(kernel_fwd1, 0, sizeof(cl_mem), (void *)&Value_clmem);
 	clStatus = clSetKernelArg(kernel_fwd1, 1, sizeof(cl_mem), (void *)&Col_clmem);
 	clStatus = clSetKernelArg(kernel_fwd1, 2, sizeof(cl_mem), (void *)&Row_Index_clmem);
+	clStatus = clSetKernelArg(kernel_fwd1, 4, sizeof(cl_mem), (void *)&koef_clmem);
 
-
-	//clStatus = clSetKernelArg(kernel_fwd2, 0, sizeof(cl_mem), (void *)&Value_clmem);
+	clStatus = clSetKernelArg(kernel_fwd2, 0, sizeof(cl_mem), (void *)&Value_clmem);
 	clStatus = clSetKernelArg(kernel_fwd2, 1, sizeof(cl_mem), (void *)&Col_clmem);
 	clStatus = clSetKernelArg(kernel_fwd2, 2, sizeof(cl_mem), (void *)&Row_Index_clmem);
-
-	clStatus = clSetKernelArg(kernel_sqr_y1, 0, sizeof(cl_mem), (void *)&Value_clmem);
-	clStatus = clSetKernelArg(kernel_sqr_y1, 1, sizeof(cl_mem), (void *)&Col_clmem);
-	clStatus = clSetKernelArg(kernel_sqr_y1, 2, sizeof(cl_mem), (void *)&Row_Index_clmem);
-	//clStatus = clSetKernelArg(kernel_sqr_y1, 3, sizeof(cl_mem), (void *)&X_clmem);
-	//clStatus = clSetKernelArg(kernel_sqr_y1, 4, sizeof(cl_mem), (void *)&Sv_clmem);
-
-	clStatus = clSetKernelArg(kernel_sqr_y2, 0, sizeof(cl_mem), (void *)&Value_clmem);
-	clStatus = clSetKernelArg(kernel_sqr_y2, 1, sizeof(cl_mem), (void *)&Col_clmem);
-	clStatus = clSetKernelArg(kernel_sqr_y2, 2, sizeof(cl_mem), (void *)&Row_Index_clmem);
-	//clStatus = clSetKernelArg(kernel_sqr_y2, 3, sizeof(cl_mem), (void *)&X_clmem);
-	//clStatus = clSetKernelArg(kernel_sqr_y2, 4, sizeof(cl_mem), (void *)&Sv_clmem);
-
-	clStatus = clSetKernelArg(kernel_sqr_x1, 0, sizeof(cl_mem), (void *)&Value_clmem);
-	clStatus = clSetKernelArg(kernel_sqr_x1, 1, sizeof(cl_mem), (void *)&Col_clmem);
-	clStatus = clSetKernelArg(kernel_sqr_x1, 2, sizeof(cl_mem), (void *)&Row_Index_clmem);
-	
-
-	clStatus = clSetKernelArg(kernel_sqr_x2, 0, sizeof(cl_mem), (void *)&Value_clmem);
-	clStatus = clSetKernelArg(kernel_sqr_x2, 1, sizeof(cl_mem), (void *)&Col_clmem);
-	clStatus = clSetKernelArg(kernel_sqr_x2, 2, sizeof(cl_mem), (void *)&Row_Index_clmem);
-
-	
-	///size_t m;
-	
-	//int count = mtx.RowIndex[N];
+	clStatus = clSetKernelArg(kernel_fwd2, 4, sizeof(cl_mem), (void *)&koef_clmem);
 
 	size_t global_size = N; // Process the entire lists
 	size_t local_size = 1; // Process one item at a time
 	
-	//m = 192 * (N/ 192);
-	//m = N * (count / N);
+
 	cout << "START" << endl;
 	for (i = 0; i < N; i++)
 	{
-		clStatus = clSetKernelArg(kernel_fwd1, 0, sizeof(cl_mem), (void *)&Value_clmem);
+		//clStatus = clSetKernelArg(kernel_fwd1, 0, sizeof(cl_mem), (void *)&Value_clmem);
 		clStatus = clSetKernelArg(kernel_fwd1, 3, sizeof(int), (void *)&i);
-		clStatus = clSetKernelArg(kernel_fwd1, 4, sizeof(cl_mem), (void *)&koef_clmem);
+		//clStatus = clSetKernelArg(kernel_fwd1, 4, sizeof(cl_mem), (void *)&koef_clmem);
 		//ToDO local_size
 		clStatus = clEnqueueNDRangeKernel(command_queue, kernel_fwd1, 1, NULL, &global_size, &local_size, 0, NULL, NULL);
 		cout << "STATUS1= "<<clStatus << endl;
-		//cout << "DONE 1" << endl << "------" << endl;
 		//TMP
 		clStatus = clEnqueueReadBuffer(command_queue, koef_clmem, CL_TRUE, 0, ((1 + N)*N) / 2 * sizeof(float), koef, 0, NULL, NULL);
-		//clStatus = clEnqueueReadBuffer(command_queue, a, CL_TRUE, 0, 1* sizeof(float), a, 0, NULL, NULL);
-		//koef[3] = -1;
 
-		//size_t tmp = 192;
-		//size_t tmp = N;
-		s[0] = 3;
-		clStatus = clSetKernelArg(kernel_fwd2, 0, sizeof(cl_mem), (void *)&Value_clmem);
+		//clStatus = clSetKernelArg(kernel_fwd2, 0, sizeof(cl_mem), (void *)&Value_clmem);
 		clStatus = clSetKernelArg(kernel_fwd2, 3, sizeof(int), (void *)&i);
-		clStatus = clSetKernelArg(kernel_fwd2, 4, sizeof(cl_mem), (void *)&koef_clmem);
-		clStatus = clSetKernelArg(kernel_fwd2, 5, sizeof(cl_mem), (void *)&s_clmem);
+		//clStatus = clSetKernelArg(kernel_fwd2, 4, sizeof(cl_mem), (void *)&koef_clmem);
 		//ToDO
 		clStatus = clEnqueueNDRangeKernel(command_queue, kernel_fwd2, 1, (0, 0, 0), &global_size, &local_size, 0, NULL, NULL);
-		//clStatus = clEnqueueNDRangeKernel(command_queue, kernel_fwd2, 1, (0, 0, 0), &m, &tmp, 0, NULL, NULL);
-		
-		//cout << "DONE 2" << endl << "------" << endl;
+
 		clStatus = clEnqueueReadBuffer(command_queue, koef_clmem, CL_TRUE, 0, ((1 + N)*N) / 2 * sizeof(float), koef, 0, NULL, NULL);
-		clStatus = clEnqueueReadBuffer(command_queue, s_clmem, CL_TRUE, 0, 1* sizeof(float), s, 0, NULL, NULL);
 		cout << "STATUS2= " << clStatus << endl;
-
-		//cout << s[0] << " S[i]"<<endl;
-
-		// если не кратно 192
-		/*if (N % 192)
-		{
-		//if (count % N) {
-			size_t tmp_1 = N % 192;
-			//size_t tmp_1 = count % N;
-			clStatus = clSetKernelArg(kernel_fwd2, 3, sizeof(int), (void *)&i);
-			//ToDO
-			//clStatus = clEnqueueNDRangeKernel(command_queue, kernel_fwd2, 1, (m, 0, 0), &global_size, &local_size, 0, NULL, NULL);
-			clStatus = clEnqueueNDRangeKernel(command_queue, kernel_fwd2, 1, (m, 0, 0), &tmp_1, &tmp_1, 0, NULL, NULL); //WORKS
-
-		cout << clStatus << endl;
-		cout << "DONE NE192" << endl << "------" << endl;*/
 	}
-	//clStatus = clEnqueueWriteBuffer(command_queue, koef_clmem, CL_TRUE, 0, ((1 + N)*N) / 2 * sizeof(float), koef, 0, NULL, NULL);
 	clStatus = clEnqueueWriteBuffer(command_queue, Sv_clmem, CL_TRUE, 0, N * sizeof(float), Sv, 0, NULL, NULL);
 		for (i = 0; i < N; i++)
 		{
-			clStatus = clSetKernelArg(kernel_sqr_y1, 3, sizeof(cl_mem), (void *)&X_clmem);
-			clStatus = clSetKernelArg(kernel_sqr_y1, 4, sizeof(cl_mem), (void *)&Sv_clmem);
-			clStatus = clSetKernelArg(kernel_sqr_y1, 5, sizeof(int), (void *)&i);
-			clStatus = clSetKernelArg(kernel_sqr_y1, 6, sizeof(cl_mem), (void *)&koef_clmem);
+			clStatus = clSetKernelArg(kernel_sqr_y1, 0, sizeof(cl_mem), (void *)&X_clmem);
+			clStatus = clSetKernelArg(kernel_sqr_y1, 1, sizeof(cl_mem), (void *)&Sv_clmem);
+			clStatus = clSetKernelArg(kernel_sqr_y1, 2, sizeof(int), (void *)&i);
+			clStatus = clSetKernelArg(kernel_sqr_y1, 3, sizeof(cl_mem), (void *)&koef_clmem);
 			//ToDO local_size
 			clStatus = clEnqueueNDRangeKernel(command_queue, kernel_sqr_y1, 1, (0, 0, 0), &global_size, &local_size, 0, NULL, NULL);
 			cout << "STATUS_sqry1= " << clStatus << endl;
 			clStatus = clEnqueueReadBuffer(command_queue, X_clmem, CL_TRUE, 0, N * sizeof(float), X, 0, NULL, NULL);
-			//clStatus = clEnqueueNDRangeKernel(command_queue, kernel_fwd2, 1, (0, 0, 0), &m, &tmp, 0, NULL, NULL);
 
-			clStatus = clSetKernelArg(kernel_sqr_y2, 3, sizeof(cl_mem), (void *)&X_clmem);
-			clStatus = clSetKernelArg(kernel_sqr_y2, 4, sizeof(cl_mem), (void *)&Sv_clmem);
-			clStatus = clSetKernelArg(kernel_sqr_y2, 5, sizeof(int), (void *)&i);
-			clStatus = clSetKernelArg(kernel_sqr_y2, 6, sizeof(cl_mem), (void *)&koef_clmem);
+			clStatus = clSetKernelArg(kernel_sqr_y2, 0, sizeof(cl_mem), (void *)&X_clmem);
+			clStatus = clSetKernelArg(kernel_sqr_y2, 1, sizeof(cl_mem), (void *)&Sv_clmem);
+			clStatus = clSetKernelArg(kernel_sqr_y2, 2, sizeof(int), (void *)&i);
+			clStatus = clSetKernelArg(kernel_sqr_y2, 3, sizeof(cl_mem), (void *)&koef_clmem);
 			clStatus = clEnqueueNDRangeKernel(command_queue, kernel_sqr_y2, 1, (0, 0, 0), &global_size, &local_size, 0, NULL, NULL);
 			cout << "STATUS_sqry_2= " << clStatus << endl;
-			/*square_y1(buffA, xcl, n + 1, i);
-			square_y2.setGlobalWorkSize(m);
-			square_y2.setGlobalWorkOffset(0, 0, 0);
-			square_y2.setLocalWorkSize(192);
-			square_y2(buffA, xcl, n + 1, i);*/
-
-			/*if (N % 192)
-			{
-				size_t tmp_1 = N % 192;
-				//size_t tmp_1 = count % N;
-				clStatus = clSetKernelArg(kernel_fwd2, 3, sizeof(int), (void *)&i);
-				//ToDO
-				//clStatus = clEnqueueNDRangeKernel(command_queue, kernel_fwd2, 1, (m, 0, 0), &global_size, &local_size, 0, NULL, NULL);
-				clStatus = clEnqueueNDRangeKernel(command_queue, kernel_fwd2, 1, (m, 0, 0), &tmp_1, &tmp_1, 0, NULL, NULL); //WORKS
-				/*square_y2.setGlobalWorkSize(n % 192);
-				square_y2.setGlobalWorkOffset(m, 0, 0);
-				square_y2.setLocalWorkSize(n % 192);
-				square_y2(buffA, xcl, n + 1, i);
-			}*/
 		}
 		for (int i = N-1; i >= 0; i--)
 		{
-			//square_x1(buffA, xcl, n + 1, i);
-			clStatus = clSetKernelArg(kernel_sqr_x1, 3, sizeof(cl_mem), (void *)&X_clmem);
-			clStatus = clSetKernelArg(kernel_sqr_x1, 4, sizeof(int), (void *)&i);
-			clStatus = clSetKernelArg(kernel_sqr_x1, 5, sizeof(cl_mem), (void *)&koef_clmem);
+			clStatus = clSetKernelArg(kernel_sqr_x1, 0, sizeof(cl_mem), (void *)&X_clmem);
+			clStatus = clSetKernelArg(kernel_sqr_x1, 1, sizeof(int), (void *)&i);
+			clStatus = clSetKernelArg(kernel_sqr_x1, 2, sizeof(cl_mem), (void *)&koef_clmem);
 			//ToDO local_size
 			clStatus = clEnqueueNDRangeKernel(command_queue, kernel_sqr_x1, 1, NULL, &global_size, &local_size, 0, NULL, NULL);
 			cout << "STATUS_sqrx_1= " << clStatus << endl;
 			clStatus = clEnqueueReadBuffer(command_queue, X_clmem, CL_TRUE, 0, N * sizeof(float), X, 0, NULL, NULL);
 			if (i > 0)
 			{
-				/*square_x2.setGlobalWorkSize(i);
-				square_x2(buffA, xcl, n + 1, i);*/
-				clStatus = clSetKernelArg(kernel_sqr_x2, 3, sizeof(cl_mem), (void *)&X_clmem);
-				clStatus = clSetKernelArg(kernel_sqr_x2, 4, sizeof(int), (void *)&i);
-				clStatus = clSetKernelArg(kernel_sqr_x2, 5, sizeof(cl_mem), (void *)&koef_clmem);
+
+				clStatus = clSetKernelArg(kernel_sqr_x2, 0, sizeof(cl_mem), (void *)&X_clmem);
+				clStatus = clSetKernelArg(kernel_sqr_x2, 1, sizeof(int), (void *)&i);
+				clStatus = clSetKernelArg(kernel_sqr_x2, 2, sizeof(cl_mem), (void *)&koef_clmem);
 				//ToDO local_size
 				clStatus = clEnqueueNDRangeKernel(command_queue, kernel_sqr_x2, 1, NULL, &global_size, &local_size, 0, NULL, NULL);
 				cout << "STATUS_sqrx_2= " << clStatus << endl;
 				clStatus = clEnqueueReadBuffer(command_queue, X_clmem, CL_TRUE, 0, N * sizeof(float), X, 0, NULL, NULL);
 			}
 		}
-	//}
-	//clStatus = clEnqueueReadBuffer(command_queue, Value_clmem, CL_TRUE, 0, local_NZ*N * sizeof(float), mtx.Value, 0, NULL, NULL);
 	clStatus = clEnqueueReadBuffer(command_queue, X_clmem, CL_TRUE, 0, local_NZ*N * sizeof(float), X, 0, NULL, NULL);
-	//clStatus = clEnqueueReadBuffer(command_queue, koef_clmem, CL_TRUE, 0, ((1 + N)*N) / 2.0 * sizeof(float), koef, 0, NULL, NULL);
-	/*cout << "FINAL VYVOD" << endl;
-	for (i = 0; i<mtx.RowIndex[N]; i++)
-	{
-		cout <<i+1<<") "<< mtx.Value[i]<<endl << "-------" << endl;
-	}*/
-
+	
 	cout << "SOLUTION" << endl;
 	for (i = 0; i<N; i++)
 	{
@@ -506,17 +343,6 @@ int main()
 		} 
 	}*/
 	Maple_check(mtx);
-	/*float tmp=0;
-	for (i = 0; i < N; i++)
-	{
-		float sum = 0;
-		for (int k = mtx.RowIndex[i]; k <=( mtx.RowIndex[i+1] - 1); k++)
-		{
-			sum += X[mtx.Col[k]] * mtx.Value[k];
-			//if (k > mtx.RowIndex[i]) tmp += X[mtx.Col[k]] * mtx.Value[k];
-		}
-		cout << sum << endl;
-	}*/
 #pragma region CleanUp
 	// Clean up and wait for all the comands to complete.
 	//все команды добавлены в очередь
@@ -528,17 +354,10 @@ int main()
 	clStatus = clReleaseKernel(kernel_fwd2);
 	clStatus = clReleaseProgram(program1);
 	clStatus = clReleaseProgram(program2);
-	clStatus = clReleaseMemObject(Value_clmem);
-	clStatus = clReleaseMemObject(Col_clmem);
-	clStatus = clReleaseMemObject(Row_Index_clmem);
 	clStatus = clReleaseCommandQueue(command_queue);
 	clStatus = clReleaseContext(context);
-	/*free(mtx.Value);
-	free(mtx.Col);
-	free(mtx.RowIndex);*/
-	FreeMemory(mtx);
-	free(platforms);
-	free(device_list);
+
+	FreeMemory(mtx, platforms, device_list, Value_clmem, Col_clmem, Row_Index_clmem);
 #pragma endregion
 
 #pragma endregion
